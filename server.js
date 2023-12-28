4a55ed0f7362fb596894b57aeddfc8c9c5013c7f
am07127 <am07127@st.habib.edu.pk>
2023-12-28 16:27:03 +0500

    small changes

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ mongoose.connect(mongoURL,{useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(cors({origin:'https://localhost:3000'}));
+app.use((req, res, next) => {
+    res.header({"Access-Control-Allow-Origin": "*"});
+    next();
+  }) 
 app.use('/api/stories',storyroutes);
 app.use('/api/auth',auth);
 app.use('/api/cases',caseroutes);
